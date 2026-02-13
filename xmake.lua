47dd313efb600e76abbb8876eb5de2b07e0aa371
Allanxa0 <allanguapoxa@gmail.com>
2026-02-13 17:12:49 -0600

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -17,7 +17,7 @@ if not has_config("vs_runtime") then
     set_runtimes("MD")
 end
 
-target("my-mod")
+target("World-Edit")
     add_rules("@levibuildscript/linkrule")
     add_rules("@levibuildscript/modpacker")
 
